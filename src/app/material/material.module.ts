133ecebee37e3785a92e9aa396ffcdfc39b202dd
Emmanuel Gonzalez <sug-040699@hotmail.com>
2023-02-07 15:29:04 -0600

    filter and sort table with paginator included.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,7 +6,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
@@ -22,7 +26,11 @@ import { MatDividerModule } from '@angular/material/divider';
     MatToolbarModule,
     MatSidenavModule,
     MatMenuModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class MaterialModule { }
--- src/app/pages/patient/patient.component.ts
+++ src/app/pages/patient/patient.component.ts
@@ -1,7 +1,9 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, ViewChild } from '@angular/core';
 import { PatientService } from '../../service/patient.service';
 import { Patient } from '../../Model/patient';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-patient',
@@ -12,6 +14,8 @@ export class PatientComponent implements OnInit {
 
   constructor(private patientService: PatientService){}
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   dataSource: MatTableDataSource<Patient>;
   displayedColumns: String[] = ['id','firstName','lastName','dni','actions'];
 
@@ -19,8 +23,15 @@ export class PatientComponent implements OnInit {
    this.patientService.findAll().subscribe(data => {
     //console.log(data);
     this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
 
    });
+  }
+  applyFilter(event: any){
+    console.log(event);
+    this.dataSource.filter = event.target.value.trim();
   }
 
 
