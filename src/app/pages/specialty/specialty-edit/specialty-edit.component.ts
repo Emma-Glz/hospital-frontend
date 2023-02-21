0b57e225323f70e62f808606df720453eb6a0785
Emmanuel Gonzalez <sug-040699@hotmail.com>
2023-02-20 23:39:32 -0600

    added table, btns, form dialog to specialty

--- src/app/pages/specialty/specialty-edit/specialty-edit.component.ts
+++ src/app/pages/specialty/specialty-edit/specialty-edit.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Specialty } from 'src/app/Model/specialty';
+import { SpecialtyService } from '../../../service/specialty.service';
 
 @Component({
   selector: 'app-specialty-edit',
   templateUrl: './specialty-edit.component.html',
   styleUrls: ['./specialty-edit.component.css']
 })
-export class SpecialtyEditComponent {
+export class SpecialtyEditComponent implements OnInit {
+  form: FormGroup;
+  id: number;
+  isEdit: boolean;
 
+  constructor(
+    private specialtyService: SpecialtyService,
+    private activedRoute: ActivatedRoute,//nos sirve para ver como se encuentra la url
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      idSpecialty: new FormControl(0),
+      nameSpecialty: new FormControl(''),
+      descriptionSpecialty: new FormControl(''),
+
+    });
+    this.activedRoute.params.subscribe(data=>{
+      this.id = data['id'];
+      this.isEdit = data['id'] != null;
+      this.initForm();
+
+    });
+
+  }
+  initForm(){
+    if(this.isEdit){
+      this.specialtyService.findById(this.id).subscribe(data=>{
+        this.form = new FormGroup({
+          idSpecialty: new FormControl(data.idSpecialty),
+          nameSpecialty: new FormControl(data.nameSpecialty),
+          descriptionSpecialty: new FormControl(data.descriptionSpecialty),
+        });
+
+      });
+
+    }
+  }
+
+  operate(){
+    const exam = new Specialty();
+    exam.idSpecialty = this.form.value['idSpecialty'];
+    exam.nameSpecialty = this.form.value['nameSpecialty'];
+    exam.descriptionSpecialty = this.form.value['descriptionSpecialty'];
+    if(this.isEdit){
+    //UPDATE
+    this.specialtyService.update(exam,exam.idSpecialty).subscribe(data => {
+      this.specialtyService.findAll().subscribe(data => {
+        //NEXT GUARDA LA DATA EN LA VARIABLE
+        this.specialtyService.setSpecialtyChange(data);
+        this.specialtyService.setMessageChange("UPDATED!");
+      })
+    });
+
+    }else{
+      //CREATE
+    this.specialtyService.save(exam).pipe(switchMap(() =>{
+      return this.specialtyService.findAll();
+    })
+    ).subscribe(data => {
+      this.specialtyService.setSpecialtyChange(data);
+      this.specialtyService.setMessageChange("CREATED!");
+
+    })
+    }
+
+    this.router.navigate(['/pages/specialty']);
+
+  }
 }
--- src/app/pages/specialty/specialty.component.ts
+++ src/app/pages/specialty/specialty.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { switchMap } from 'rxjs';
+import { SpecialtyService } from 'src/app/service/specialty.service';
+import { Specialty } from '../../Model/specialty';
 
 @Component({
   selector: 'app-specialty',
   templateUrl: './specialty.component.html',
   styleUrls: ['./specialty.component.css']
 })
-export class SpecialtyComponent {
+export class SpecialtyComponent implements OnInit{
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(
+    private specialtyService: SpecialtyService,
+    private _SnackBar: MatSnackBar
+    ){}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: MatTableDataSource<Specialty>;
+  displayedColumns: String[] = ['id','NameSpecialty','DescriptionSpecialty','actions'];
+
+  ngOnInit(): void {
+    //con estas lines reaccionamos
+    this.specialtyService.getSpecialtyChange().subscribe(data =>{
+      this.createTable(data);
+    });
+
+    this.specialtyService.getMessageChange().subscribe(data =>{
+        this._SnackBar.open(data,"INFO",{
+          horizontalPosition: this.horizontalPosition,
+          verticalPosition: this.verticalPosition,
+          duration:2000});
+    });
+
+   this.specialtyService.findAll().subscribe(data => {
+    //console.log(data);
+    this.createTable(data);
+
+   });
+  }
+  applyFilter(event: any){
+    this.dataSource.filter = event.target.value.trim();//trim elimina los espacios al inicio y final
+  }
+  createTable(data:Specialty[]){
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
+  }
+  delete(id: number){
+    this.specialtyService.delete(id).pipe(switchMap(() => {
+      return this.specialtyService.findAll()
+    }))
+    .subscribe(data => {
+      //this.createTable(data);
+      //internamente se esta hacinedo un .NEXT para gurdar la data en esa variable reactiva.
+      this.specialtyService.setSpecialtyChange(data);
+      this.specialtyService.setMessageChange("DELETED!");
+    })
+  }
 
 }
