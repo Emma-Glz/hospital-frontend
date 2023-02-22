946e532df9126ef1cf4b0e54b514318589acb399
Emmanuel Gonzalez <sug-040699@hotmail.com>
2023-02-21 19:51:36 -0600

    register consult menu. Models, component and import select to work with angular material Method 2 note: In this method I use Observable of rxjs

--- src/app/pages/consult/consult.component.ts
+++ src/app/pages/consult/consult.component.ts
@@ -3,6 +3,8 @@ import { PatientService } from '../../service/patient.service';
 import { MedicService } from '../../service/medic.service';
 import { SpecialtyService } from '../../service/specialty.service';
 import { Patient } from 'src/app/Model/patient';
+import { Observable } from 'rxjs';
+import { ExamService } from '../../service/exam.service';
 
 @Component({
   selector: 'app-consult',
@@ -11,17 +13,16 @@ import { Patient } from 'src/app/Model/patient';
 })
 export class ConsultComponent implements OnInit {
   idPatientSeleted: number;
-  patients: Patient[] = [];
+  patients$: Observable<Patient[]>;
 
   constructor(
     private patientService: PatientService,
     private medicService: MedicService,
-    private specialtyService: SpecialtyService
+    private specialtyService: SpecialtyService,
+    private examSpecialty: ExamService
   ) { }
   ngOnInit(): void {
-    this.patientService.findAll().subscribe(data => {
-      this.patients = data;
-    })
+    this.patients$ = this.patientService.findAll();
   }
 
 
