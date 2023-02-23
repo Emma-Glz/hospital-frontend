36d1ef3aa9c3836635f5e01738701b6a507ed130
Emmanuel Gonzalez <sug-040699@hotmail.com>
2023-02-23 10:32:58 -0600

    -input diagnosis, treatment works with his btn  Add Detail and Remove Detail. -select exam adder with mat-options acording db and captured in list, it was validated if the user don't choose an option.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -17,6 +17,8 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { DateAdapter, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { CustomDateAdapter } from './custom-adapter';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -41,7 +43,9 @@ import { CustomDateAdapter } from './custom-adapter';
     MatDialogModule,
     MatSelectModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatListModule
 
   ],
   providers:[
--- src/app/pages/consult/consult.component.ts
+++ src/app/pages/consult/consult.component.ts
@@ -8,6 +8,8 @@ import { ExamService } from '../../service/exam.service';
 import { Medic } from 'src/app/Model/medic';
 import { Specialty } from '../../Model/specialty';
 import { Exam } from 'src/app/Model/exam';
+import { ConsultDetail } from 'src/app/Model/consultDetail';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-consult',
@@ -18,19 +20,28 @@ export class ConsultComponent implements OnInit {
   idPatientSeleted: number;
   idMedicSeleted: number;
   idSpecialtySeleted: number;
-
+  idExamSeleted: number;
+  dateSeleted: Date;
 
   patients$: Observable<Patient[]>;
   medics$: Observable<Medic[]>;
   specialties$: Observable<Specialty[]>;
   exams$: Observable<Exam[]>;
 
+  details: ConsultDetail[] = [];
+  examsSelected: Exam[] = [];
+
   minDate: Date = new Date();
+
+  diagnosis: string;
+  treatment: string;
+
   constructor(
     private patientService: PatientService,
     private medicService: MedicService,
     private specialtyService: SpecialtyService,
-    private examSpecialty: ExamService
+    private examService: ExamService,
+    private _snackBar: MatSnackBar
   ) { }
   ngOnInit(): void {
     this.initData();
@@ -40,7 +51,7 @@ export class ConsultComponent implements OnInit {
     this.patients$ = this.patientService.findAll();
     this.medics$ = this.medicService.findAll();
     this.specialties$ = this.specialtyService.findAll();
-    this.exams$ = this.examSpecialty.findAll();
+    this.exams$ = this.examService.findAll();
   }
   OnDateChange(e: any) {
     console.log(e)
@@ -49,6 +60,24 @@ export class ConsultComponent implements OnInit {
     console.log(e)
   }
 
+  addDetail() {
+    let det = new ConsultDetail();
+    det.diagnosis = this.diagnosis;
+    det.treatment = this.treatment;
+    this.details.push(det);
+  }
 
+  removeDetail( index: number){
+    this.details.splice(index,1);
+  }
+  addExams(){
+    if(this.idExamSeleted > 0){
+      this.examService.findById(this.idExamSeleted).subscribe(data => this.examsSelected.push(data))
+
+    }else{
+      this._snackBar.open("Please select an exam", 'INFO',{duration:2000});
+    }
+
+  }
 
 }
